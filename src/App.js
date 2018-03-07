dcff740e04fb46984b4cb919ab8645396222483f
github username <github email address>
2018-03-07 13:24:40 -0500

    base auth set up, no back end support yet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Login from './Login';
+import Profile from './Profile';
+import * as actions from './actions';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>Welcome to the App, you cant do anything unless you login</h2>
+        <ul>
+          <li>
+            {this.props.loggedIn ? (
+              <a
+                onClick={e => {
+                  e.preventDefault();
+                  this.props.logoutUser();
+                }}
+              >
+                Sign Out
+              </a>
+            ) : (
+              <Link to="/login">Go to Login</Link>
+            )}
+          </li>
+          <li>
+            <Link to="/profile">Go to Profile</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
       </div>
     );
   }
 }
-
-export default App;
+const mapStateToProps = state => ({
+  loggedIn: !!state.auth.currentUser.id
+});
+export default connect(mapStateToProps, actions)(App);
--- src/index.js
+++ src/index.js
@@ -7,7 +7,35 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
 
+import App from './App';
 
+const initialState = { currentUser: {} };
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      const { id, username } = action.user;
+      return { ...state, currentUser: { id, username } };
+    case 'LOGOUT_USER':
+      return { ...state, currentUser: {} };
+    default:
+      return state;
+  }
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootReducer = combineReducers({
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+
+const Root = ({ store }) => {
+  return (
+    <Router>
+      <Provider store={store}>
+        <Route path="/" component={App} />
+      </Provider>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
